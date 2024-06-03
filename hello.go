f896dc82dbc83db80280d430b2a90f3a15e83c7f
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-03 16:48:35 +0200

    update

--- hello.go
+++ hello.go
@@ -11,4 +11,11 @@ func main() {
 
     flag.Parse()
     fmt.Println("# flag.Args(): ", flag.Args())
+
+    path, err := os.Executable()
+    if err == nil {
+        fmt.Println("# os.Executable(): ", path)
+    } else {
+        fmt.Println("# os.Executable(): Error")
+    }
 }
