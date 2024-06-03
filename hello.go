946e7e28bade921034b66adde70b5951ad0d22b3
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-03 17:00:27 +0200

    a

--- hello.go
+++ hello.go
@@ -4,6 +4,7 @@ import (
     "flag"
     "fmt"
     "os"
+    "syscall"
 )
 
 func main() {
@@ -17,5 +18,12 @@ func main() {
         fmt.Println("# os.Executable(): ", path)
     } else {
         fmt.Println("# os.Executable(): Error")
+    }
+
+    id, _, _ := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+    if id == 0 {
+        fmt.Println("In child, args=", os.Args)
+    } else {
+        fmt.Println("In parent, args=", os.Args)
     }
 }
